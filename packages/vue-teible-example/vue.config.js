8f57e5b86e6f368015ae1b797ec2d988aa5c5e7b
hiendv <hien.dv.neo@gmail.com>
2018-07-19 18:01:18 +0700

    vue-teible-example: refactor

--- packages/vue-teible-example/vue.config.js
+++ packages/vue-teible-example/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/teible' : ''
+  baseUrl: process.env.NODE_ENV === 'production' ? '/teible' : '',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'src/index.html'
+    }
+  }
 }
