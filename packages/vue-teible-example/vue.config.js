92364bdf88a695e3948584727573ad43d6940e1d
hiendv <hien.dv.neo@gmail.com>
2018-09-27 11:18:44 +0700

    update dependencies & vue-teible-example: remove uglifyjs-webpack-plugin

--- packages/vue-teible-example/vue.config.js
+++ packages/vue-teible-example/vue.config.js
@@ -1,15 +1,9 @@
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '/teible' : '',
   pages: {
     index: {
       entry: 'src/main.js',
       template: 'src/index.html'
-    }
-  },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(new UglifyJsPlugin())
     }
   }
 }
