e7e723a0d675e20d0e5052531af40836b5d36be6
hiendv <hien.dv.neo@gmail.com>
2018-11-02 09:29:19 +0700

    fix(vue-teible-example): update lint config

--- packages/vue-teible-example/.eslintrc.js
+++ packages/vue-teible-example/.eslintrc.js
@@ -9,7 +9,11 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/max-attributes-per-line': [2, {
+      'singleline': 3,
+      'multiline': 3
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint'
