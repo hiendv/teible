b3ad42930670de75dad5eb2050865880da72532b
hiendv <hien.dv.neo@gmail.com>
2019-04-22 19:08:13 +0700

    chore(vue-teible-example): update deps

--- packages/vue-teible-example/vue.config.js
+++ packages/vue-teible-example/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' && !process.env.TEIBLE_EXAMPLE ? '/teible' : '',
+  publicPath: process.env.NODE_ENV === 'production' && !process.env.TEIBLE_EXAMPLE ? '/teible' : '',
   pages: {
     index: {
       entry: 'src/main.js',
