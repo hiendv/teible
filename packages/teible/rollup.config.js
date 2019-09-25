d12e0b5b8d73a032ccd3c0b54f16f1c73ffa07a4
hiendv <hien.dv.neo@gmail.com>
2019-09-25 15:03:10 +0700

    chore(teible): remove the iife output

--- packages/teible/rollup.config.js
+++ packages/teible/rollup.config.js
@@ -1,6 +1,4 @@
 import path from 'path'
-import postcss from 'rollup-plugin-postcss'
-import buble from 'rollup-plugin-buble'
 
 const reslv = p => {
   return path.resolve(__dirname, p)
@@ -14,14 +12,5 @@ export default [
       file: reslv('dist/es.js')
     },
     external: id => /\.scss$/.test(id)
-  },
-  {
-    input: reslv('src/index.js'),
-    output: {
-      name: 'tabs',
-      format: 'iife',
-      file: reslv('dist/iife.js')
-    },
-    plugins: [postcss(), buble()]
   }
 ]
