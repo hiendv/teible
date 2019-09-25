a3c7301907e3679ac81202acb29c810380d825c4
hiendv <hien.dv.neo@gmail.com>
2019-09-25 15:02:25 +0700

    chore(vue-teible-example): correct publicPath

--- packages/vue-teible-example/vue.config.js
+++ packages/vue-teible-example/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' && process.env.GH_PAGES ? '/tabs' : ''
+  publicPath: process.env.NODE_ENV === 'production' && process.env.GH_PAGES ? '/teible' : ''
 }
