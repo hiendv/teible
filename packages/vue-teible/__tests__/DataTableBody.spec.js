3b1ed8d2742afbcedd58ac05ab2bf0afab9ea7cf
hiendv <hien.dv.neo@gmail.com>
2020-07-15 10:25:39 +0700

    test(vue-teible): update DataTableBody tests

--- packages/vue-teible/__tests__/DataTableBody.spec.js
+++ packages/vue-teible/__tests__/DataTableBody.spec.js
@@ -20,7 +20,8 @@ describe('DataTableBody', () => {
           field: 'two',
           label: 'Two'
         }],
-        click: () => {}
+        click: () => {},
+        hover: () => {}
       },
       provide: () => ({
         $theme: () => idObj
@@ -28,5 +29,81 @@ describe('DataTableBody', () => {
     })
 
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('accepts click', () => {
+    const click = jest.fn()
+
+    const wrapper = shallowMount(DataTableBody, {
+      propsData: {
+        items: [{
+          one: 'foo',
+          two: 'bar'
+        }, {
+          one: 'baz',
+          two: 'qux'
+        }],
+        columns: [{
+          field: 'one',
+          label: 'One'
+        }, {
+          field: 'two',
+          label: 'Two'
+        }],
+        click,
+        hover: () => {}
+      },
+      provide: () => ({
+        $theme: () => idObj
+      })
+    })
+
+    wrapper.find('tr').trigger('click')
+    return wrapper.vm.$nextTick()
+      .then(() => {
+        expect(click.mock.calls.length).toBe(1)
+        expect(click.mock.calls[0][0]).toBeInstanceOf(MouseEvent)
+        expect(click.mock.calls[0][0].type).toBe('click')
+        expect(click.mock.calls[0][1]).toStrictEqual({ one: 'foo', two: 'bar' })
+        expect(click.mock.calls[0][2]).toStrictEqual(0)
+      })
+  })
+
+  it('accepts hover', () => {
+    const hover = jest.fn()
+
+    const wrapper = shallowMount(DataTableBody, {
+      propsData: {
+        items: [{
+          one: 'foo',
+          two: 'bar'
+        }, {
+          one: 'baz',
+          two: 'qux'
+        }],
+        columns: [{
+          field: 'one',
+          label: 'One'
+        }, {
+          field: 'two',
+          label: 'Two'
+        }],
+        click: () => {},
+        hover
+      },
+      provide: () => ({
+        $theme: () => idObj
+      })
+    })
+
+    wrapper.find('tr').trigger('hover')
+    return wrapper.vm.$nextTick()
+      .then(() => {
+        expect(hover.mock.calls.length).toBe(1)
+        expect(hover.mock.calls[0][0]).toBeInstanceOf(Event)
+        expect(hover.mock.calls[0][0].type).toBe('hover')
+        expect(hover.mock.calls[0][1]).toStrictEqual({ one: 'foo', two: 'bar' })
+        expect(hover.mock.calls[0][2]).toStrictEqual(0)
+      })
   })
 })
